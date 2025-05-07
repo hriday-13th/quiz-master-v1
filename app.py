bf02bba141abd6d94c38ea2c9273822689134004
hriday-13th <hridaypradhan201003@gmail.com>
2025-05-07 12:29:24 +0530

    Add: Added template files, restructured app.py

--- app.py
+++ app.py
@@ -1,16 +1,61 @@
-from flask import Flask, request, jsonify, redirect_url
+import uvicorn
+from asgiref.wsgi import WsgiToAsgi
+from flask import Flask, request, jsonify, redirect, render_template, session, g
+
+import sqlite3, hashlib, os
 
 app = Flask(__name__)
+app.secret_key = "password"
+DATABASE = "quizmaster.db"
 
+
+# -- database functions --
+def get_db():
+    if 'db' not in g:
+        g.db = DATABASE
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+    with app.open_resource('sql/schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+# -- password hashing --
+def hash_password(password):
+    return hashlib.sha256(password.encode().hexdigest())
+
+
+# -- app functions --
 @app.route('/')
 def home():
-    redirect_url("/login")
+    return redirect("/login")
 
-@app.route('/login'):
+@app.route('/signup')
+def signup():
+    return render_template("signup_page.html")
+
+@app.route('/login')
 def login():
-    return "we logging here"
+    return render_template("login_page.html")
 
-# Example API route (GET + POST)
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect("/login")
+    return render_template("dashboard.html")
+
+@app.route('/admin_dashboard')
+def admin_dashboard():
+    pass
+
 @app.route('/api/data', methods=['GET', 'POST'])
 def data():
     if request.method == 'GET':
@@ -19,7 +64,16 @@ def data():
         data = request.get_json()
         return jsonify({"message": "You sent a POST request!", "data": data})
 
-# Run the server
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+asgi_app = WsgiToAsgi(app)
 
+if __name__ == '__main__':
+    # if not os.path.exists(DATABASE):
+    #     with app.app_context():
+    #         init_db()
+
+    uvicorn.run(
+        "app:asgi_app",
+        host="0.0.0.0",
+        port=9000,
+        reload=True
+    )
---
+++ templates/login_page.html
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Quiz-Master</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <style>
+      body, html {
+        height: 100%;
+        margin: 0;
+      }
+    </style>
+  </head>
+
+  <body class="d-flex justify-content-center align-items-center">
+    <div class="container">
+      <div class="p-4 border border-black rounded" style="max-width: 400px; margin: auto;">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 text-center">
+            <a href="/signup">Don't have an account? Sign up</a>
+          </div>          
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  </body>
+</html>
